0797ad588961c11d22823816ca3fcd8c6fda3a42
sirin-acciojob-26 <166235930+sirin-acciojob-26@users.noreply.github.com>
2024-07-09 21:35:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 // Define the Person class
 class Person {
-  constructor(name, age) {
-    this._name = name;
-    this._age = age;
-  }
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
 
-  get name() {
-    return this._name;
-  }
+    get name() {
+        return this._name;
+    }
 
-  set age(newAge) {
-    this._age = newAge;
-  }
+    set age(age) {
+        this._age = age;
+    }
 
-  get age() {
-    return this._age;
-  }
+    get age() {
+        return this._age;
+    }
 }
 
+// Define the Student class that extends Person
 class Student extends Person {
-  study() {
-    console.log(`${this._name} is studying`);
-  }
+    study() {
+        console.log(`${this.name} is studying`);
+    }
 }
 
+// Define the Teacher class that extends Person
 class Teacher extends Person {
-  teach() {
-    console.log(`${this._name} is teaching`);
-  }
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
 }
